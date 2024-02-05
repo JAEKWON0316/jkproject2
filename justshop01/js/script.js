7e2ba929972ba31ead0f1ff664ab2f187a169a84
JK Lee <149368895+JAEKWON0316@users.noreply.github.com>
2024-02-05 18:13:25 +0900

    Add files via upload

--- justshop01/js/script.js
+++ justshop01/js/script.js
@@ -4,5 +4,14 @@ $(function(){
         $('.lnb').toggle();
     });
 
+    $('.tabnav>li').click(function(e){
+        e.preventDefault();
+        $('.tabnav>li').removeClass('active');
+        const href = $(this).find('a').attr('href'); //갤러리와 노티스의 id값 href변수에 받음
+        $(this).addClass('active');
+        $('.tab').removeClass('active');
+        $(href).addClass('active');    
+    });
+
 
 }); //jquery
