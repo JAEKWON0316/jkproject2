9f35732de9999352e3c66b73d85d9155ace86c39
JK Lee <149368895+JAEKWON0316@users.noreply.github.com>
2024-02-07 18:14:03 +0900

    Add files via upload

--- webExam/justshop01/js/script.js
+++ webExam/justshop01/js/script.js
@@ -1,9 +1,37 @@
 $(function(){
 
-    $('.mainnav>li').hover(function(){
-        $('.submenu').toggle();
-      
+    $('.gnb').hover(function(){
+        $('.lnb').toggle();
     });
 
+    $('.tabnav>li').click(function(e){
+        e.preventDefault();
+        $('.tabnav>li').removeClass('active');
+        const href = $(this).find('a').attr('href'); //갤러리와 노티스의 id값 href변수에 받음
+        $(this).addClass('active');
+        $('.tab').removeClass('active');
+        $(href).addClass('active');    
+    });
+    setInterval(slideUpDown, 3000); //setInterval == 함수 실행 메소드 , 시간 3초에 1번씩 실행되도록
 
+    $('.pop').click(function(e){
+        e.preventDefault(); //a태그의 기능을 죽인다.
+        $('.popup').fadeIn();
+    });
+    $('.close').click(function(){
+        $('.popup').fadeOut();
+    });
 }); //jquery
+
+function slideUpDown(){ //함수명
+    $('.heroin').animate({
+        top: '-300px' //1번째 이미지를 위로 -300만큼 애니메이션 한다.
+    }, 300, function(){ //이미지가 넘어가는속도가 0.3초
+        //애니메이션이 끝나면 두 번재 이미지가 보이게 된다. 이때 첫 번째 이미지를
+        //복제(clone)하여 마지막 순서로 만들고(appendTo) 첫 번째 이미지를 삭제하여(remove)
+        //그 다음 이미지(2번째)가 1번째가 되게한다. 그리고 top을 초기화 해놓는다.(계속 -300px씩 하니까.)
+        $('.heroin img:eq(0)').clone().appendTo('.heroin'); //1번째거를 박스 끝에 붙혀줌
+        $('.heroin img:eq(0)').remove(); //1번째거를 사라지게함
+        $('.heroin').css("top", 0); // 탑을 0으로 바꿈 즉,, 123 순을 231로 바꿈
+    });
+}
